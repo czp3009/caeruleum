ec7569a344a8560eac503ef1e28dd3c5db2d1558
czp3009 <czp3009@gmail.com>
2019-04-19 19:15:31 +0800

    Optimize code

--- src/main/kotlin/com/hiczp/caeruleum/Caeruleum.kt
+++ src/main/kotlin/com/hiczp/caeruleum/Caeruleum.kt
@@ -9,12 +9,11 @@ import java.lang.reflect.Modifier
 import java.lang.reflect.Proxy
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineContext
 import kotlin.reflect.KClass
 import kotlin.reflect.jvm.kotlinFunction
 
-
-private val serviceFunctionCache = ConcurrentHashMap<Method, ServiceFunction>()
-private val nonAbstractKotlinFunctionCache = ConcurrentHashMap<Method, Method>()
+private val cache = ConcurrentHashMap<Method, (Array<Any?>?) -> Any>()
 
 @PublishedApi
 internal fun dynamicProxyToHttpClient(kClass: KClass<*>, httpClient: HttpClient): Any {
@@ -23,66 +22,87 @@ internal fun dynamicProxyToHttpClient(kClass: KClass<*>, httpClient: HttpClient)
     if (javaClass.interfaces.isNotEmpty()) throw IllegalArgumentException("API interfaces must not extend other interfaces")
 
     return Proxy.newProxyInstance(javaClass.classLoader, arrayOf(javaClass)) { proxy, method, args ->
-        //if isSynthetic
-        val kFunction = method.kotlinFunction ?: return@newProxyInstance method.invoke(proxy, *args.orEmpty())
+        cache.getOrPut(method) {
+            val kFunction = method.kotlinFunction
+            when {
+                //if isSynthetic
+                kFunction == null -> {
+                    { method.invoke(proxy, *it.orEmpty()) }
+                }
 
-        //non-abstract method
-        if (!kFunction.isAbstract) {
-            //non-abstract in JVM so that it's Default Method
-            return@newProxyInstance if (!Modifier.isAbstract(method.modifiers)) {
-                // Because the service interface might not be public, we need to use a MethodHandle lookup
-                // that ignores the visibility of the declaringClass
-                MethodHandles.Lookup::class.java.getDeclaredConstructor(
-                    Class::class.java,
-                    Int::class.javaPrimitiveType
-                ).apply {
-                    setAccessible(true)
-                }.newInstance(javaClass, -1)
-                    .unreflectSpecial(method, javaClass)
-                    .bindTo(proxy)
-                    .invokeWithArguments(*args.orEmpty())
-            } else { //non-abstract kotlin function
-                nonAbstractKotlinFunctionCache.getOrPut(method) {
-                    javaClass.declaredClasses.find {
-                        it.simpleName == "DefaultImpls"
-                    }!!.getDeclaredMethod(
-                        method.name,
-                        javaClass,
-                        *method.parameterTypes
-                    )
-                }.invoke(null, proxy, *args.orEmpty())
-            }
-        }
+                //non-abstract method
+                !kFunction.isAbstract -> {
+                    //default method
+                    if (!Modifier.isAbstract(method.modifiers)) {
+                        // Because the service interface might not be public, we need to use a MethodHandle lookup
+                        // that ignores the visibility of the declaringClass
+                        val methodHandles = MethodHandles.Lookup::class.java.getDeclaredConstructor(
+                            Class::class.java,
+                            Int::class.javaPrimitiveType
+                        ).apply {
+                            setAccessible(true)
+                        }
 
-        //method in Object
-        if (method.declaringClass == Any::class.java) {
-            return@newProxyInstance when (method.name) {
-                "equals" -> when {
-                    args == null -> false
-                    args[0] === proxy -> true
-                    args[0] !is Proxy -> false
-                    else -> args[0].javaClass.interfaces.let {
-                        if (it.size != 1) false else it[0] == javaClass
+                        fun(args: Array<Any?>?) = methodHandles.newInstance(javaClass, -1)
+                            .unreflectSpecial(method, javaClass)
+                            .bindTo(proxy)
+                            .invokeWithArguments(*args.orEmpty())
+                    } else { //non-abstract kotlin function
+                        val realMethod = javaClass.declaredClasses.find {
+                            it.simpleName == "DefaultImpls"
+                        }!!.getDeclaredMethod(
+                            method.name,
+                            javaClass,
+                            *method.parameterTypes
+                        )
+
+                        fun(args: Array<Any?>?) = realMethod.invoke(null, proxy, *args.orEmpty())
                     }
                 }
-                "hashCode" -> kClass.qualifiedName.hashCode()
-                "toString" -> "Service interface ${kClass.qualifiedName}"
-                else -> null    //Impossible
+
+                //method in Object
+                method.declaringClass == Any::class.java -> when (method.name) {
+                    "equals" -> fun(args: Array<Any?>?) = when {
+                        args == null -> false
+                        args[0] === proxy -> true
+                        args[0] !is Proxy -> false
+                        else -> args[0]!!.javaClass.interfaces.let {
+                            if (it.size != 1) false else it[0] == javaClass
+                        }
+                    }
+                    "hashCode" -> {
+                        { kClass.qualifiedName.hashCode() }
+                    }
+                    "toString" -> {
+                        { "Service interface ${kClass.qualifiedName}" }
+                    }
+                    else -> {
+                        { Unit }  //Impossible
+                    }
+                }
+
+                else -> {
+                    val serviceFunction = ServiceFunction(kClass, kFunction)
+
+                    fun(args: Array<Any?>?): Any {
+                        val realArgs: Array<Any?>?
+                        val coroutineContext: CoroutineContext
+                        if (serviceFunction.isSuspend) {
+                            realArgs = args!!.copyOf(args.size - 1)
+                            coroutineContext = (args.last() as Continuation<*>).context
+                        } else {
+                            realArgs = args ?: emptyArray()
+                            coroutineContext = httpClient.coroutineContext
+                        }
+
+                        return CoroutineScope(coroutineContext).async {
+                            httpClient.execute(serviceFunction.httpRequestBuilder(realArgs))
+                                .receive(serviceFunction.returnTypeInfo)
+                        }
+                    }
+                }
             }
-        }
-
-        val serviceFunction = serviceFunctionCache.getOrPut(method) { ServiceFunction(kClass, kFunction) }
-        val realArgs = if (serviceFunction.isSuspend) args.copyOf(args.size - 1) else args ?: emptyArray()
-        val coroutineContext = if (serviceFunction.isSuspend) {
-            (args.last() as Continuation<*>).context
-        } else {
-            httpClient.coroutineContext
-        }
-
-        CoroutineScope(coroutineContext).async {
-            httpClient.execute(serviceFunction.httpRequestBuilder(realArgs))
-                .receive(serviceFunction.returnTypeInfo)
-        }
+        }(args)
     }
 }
 
