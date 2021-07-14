b55a302485cf7e909a41f76d8c843ed11f2fa7d3
czp3009 <czp3009@gmail.com>
2021-07-14 17:07:00 +0800

    cache analysed method

--- src/main/kotlin/com/hiczp/caeruleum/Caeruleum.kt
+++ src/main/kotlin/com/hiczp/caeruleum/Caeruleum.kt
@@ -1,104 +1,125 @@
 package com.hiczp.caeruleum
 
 import io.ktor.client.*
-import io.ktor.client.statement.*
-import io.ktor.util.reflect.*
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.async
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
-import java.lang.invoke.MethodHandles
-import java.lang.reflect.Modifier
-import java.lang.reflect.Proxy.newProxyInstance
-import kotlin.coroutines.Continuation
-import kotlin.coroutines.intrinsics.COROUTINE_SUSPENDED
+import java.lang.reflect.Proxy
 import kotlin.reflect.KClass
-import kotlin.reflect.jvm.kotlinFunction
 
-@PublishedApi
-internal fun dynamicProxyToHttpClient(kClass: KClass<*>, httpClient: HttpClient, baseUrl: String?): Any {
-    val javaClass = kClass.java
-    require(javaClass.isInterface) { "API declarations must be interfaces" }
-    require(javaClass.interfaces.isEmpty()) { "API interfaces must not extend other interfaces" }
-
-    return newProxyInstance(javaClass.classLoader, arrayOf(javaClass)) { proxy, method, args: Array<Any>? ->
-        if (!httpClient.isActive) throw CancellationException("Parent context in HttpClient is cancelled")
-
-        val kFunction = method.kotlinFunction
-        when {
-            //if isSynthetic
-            kFunction == null -> method(proxy, *args.orEmpty())
-
-            //method in Object
-            method.declaringClass == Any::class.java -> when (method.name) {
-                "equals" -> when {
-                    args == null -> false
-                    args[0] === proxy -> true
-                    args[0] !is java.lang.reflect.Proxy -> false
-                    else -> args[0].javaClass.interfaces.let {
-                        if (it.size != 1) false else it[0] == javaClass
-                    }
-                }
-                "hashCode" -> kClass.qualifiedName.hashCode()
-                "toString" -> "Service interface ${kClass.qualifiedName}"
-                else -> Unit   //Impossible
-            }
-
-            //non-abstract method
-            !kFunction.isAbstract -> {
-                //default method
-                if (!Modifier.isAbstract(method.modifiers)) {
-                    // Because the service interface might not be public, we need to use a MethodHandle lookup
-                    // that ignores the visibility of the declaringClass
-                    MethodHandles.Lookup::class.java.getDeclaredConstructor(
-                        Class::class.java,
-                        Int::class.javaPrimitiveType
-                    ).apply {
-                        setAccessible(true)
-                    }.newInstance(javaClass, -1)
-                        .unreflectSpecial(method, javaClass)
-                        .bindTo(proxy)
-                        .invokeWithArguments(*args.orEmpty())
-                } else { //non-abstract kotlin function
-                    javaClass.declaredClasses.find {
-                        it.simpleName == "DefaultImpls"
-                    }!!.getDeclaredMethod(
-                        method.name,
-                        javaClass,
-                        *method.parameterTypes
-                    )(null, proxy, *args.orEmpty())
-                }
-            }
-
-            else -> {
-                val serviceFunction = ServiceFunction(kClass, kFunction)
-                if (serviceFunction.isSuspend) {
-                    @Suppress("UNCHECKED_CAST")
-                    val continuation = args!!.last() as Continuation<Any>
-                    val realArgs = args.copyOf(args.size - 1)
-                    httpClient.launch {
-                        runCatching {
-                            HttpStatement(serviceFunction.httpRequestBuilder(baseUrl, realArgs), httpClient)
-                                .executeAndReceive(serviceFunction.returnTypeInfo)
-                        }.run(continuation::resumeWith)
-                    }
-                    COROUTINE_SUSPENDED
-                } else {
-                    httpClient.async {
-                        HttpStatement(serviceFunction.httpRequestBuilder(baseUrl, args.orEmpty()), httpClient)
-                            .executeAndReceive(serviceFunction.returnTypeInfo)
-                    }
-                }
-            }
-        }
-    }
-}
+//import io.ktor.client.*
+//import io.ktor.client.statement.*
+//import io.ktor.util.reflect.*
+//import kotlinx.coroutines.CancellationException
+//import kotlinx.coroutines.async
+//import kotlinx.coroutines.isActive
+//import kotlinx.coroutines.launch
+//import java.lang.invoke.MethodHandles
+//import java.lang.reflect.Modifier
+//import java.lang.reflect.Proxy.newProxyInstance
+//import kotlin.coroutines.Continuation
+//import kotlin.coroutines.intrinsics.COROUTINE_SUSPENDED
+//import kotlin.reflect.KClass
+//import kotlin.reflect.jvm.kotlinFunction
+//
+//@PublishedApi
+//internal fun dynamicProxyToHttpClient(kClass: KClass<*>, httpClient: HttpClient, baseUrl: String?): Any {
+//    val javaClass = kClass.java
+//    require(javaClass.isInterface) { "API declarations must be interfaces" }
+//    require(javaClass.interfaces.isEmpty()) { "API interfaces must not extend other interfaces" }
+//
+//    return newProxyInstance(javaClass.classLoader, arrayOf(javaClass)) { proxy, method, args: Array<Any>? ->
+//        if (!httpClient.isActive) throw CancellationException("Parent context in HttpClient is cancelled")
+//
+//        val kFunction = method.kotlinFunction
+//        when {
+//            //if isSynthetic
+//            kFunction == null -> method(proxy, *args.orEmpty())
+//
+//            //method in Object
+//            method.declaringClass == Any::class.java -> when (method.name) {
+//                "equals" -> when {
+//                    args == null -> false
+//                    args[0] === proxy -> true
+//                    args[0] !is java.lang.reflect.Proxy -> false
+//                    else -> args[0].javaClass.interfaces.let {
+//                        if (it.size != 1) false else it[0] == javaClass
+//                    }
+//                }
+//                "hashCode" -> kClass.qualifiedName.hashCode()
+//                "toString" -> "Service interface ${kClass.qualifiedName}"
+//                else -> Unit   //Impossible
+//            }
+//
+//            //non-abstract method
+//            !kFunction.isAbstract -> {
+//                //default method
+//                if (!Modifier.isAbstract(method.modifiers)) {
+//                    // Because the service interface might not be public, we need to use a MethodHandle lookup
+//                    // that ignores the visibility of the declaringClass
+//                    MethodHandles.Lookup::class.java.getDeclaredConstructor(
+//                        Class::class.java,
+//                        Int::class.javaPrimitiveType
+//                    ).apply {
+//                        setAccessible(true)
+//                    }.newInstance(javaClass, -1)
+//                        .unreflectSpecial(method, javaClass)
+//                        .bindTo(proxy)
+//                        .invokeWithArguments(*args.orEmpty())
+//                } else { //non-abstract kotlin function
+//                    javaClass.declaredClasses.find {
+//                        it.simpleName == "DefaultImpls"
+//                    }!!.getDeclaredMethod(
+//                        method.name,
+//                        javaClass,
+//                        *method.parameterTypes
+//                    )(null, proxy, *args.orEmpty())
+//                }
+//            }
+//
+//            else -> {
+//                val serviceFunction = ServiceFunction(kClass, kFunction)
+//                if (serviceFunction.isSuspend) {
+//                    @Suppress("UNCHECKED_CAST")
+//                    val continuation = args!!.last() as Continuation<Any>
+//                    val realArgs = args.copyOf(args.size - 1)
+//                    httpClient.launch {
+//                        runCatching {
+//                            HttpStatement(serviceFunction.httpRequestBuilder(baseUrl, realArgs), httpClient)
+//                                .executeAndReceive(serviceFunction.returnTypeInfo)
+//                        }.run(continuation::resumeWith)
+//                    }
+//                    COROUTINE_SUSPENDED
+//                } else {
+//                    httpClient.async {
+//                        HttpStatement(serviceFunction.httpRequestBuilder(baseUrl, args.orEmpty()), httpClient)
+//                            .executeAndReceive(serviceFunction.returnTypeInfo)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//inline fun <reified T> HttpClient.create(baseUrl: String? = null) =
+//    dynamicProxyToHttpClient(T::class, this, baseUrl) as T
+//
+//private suspend inline fun HttpStatement.executeAndReceive(returnTypeInfo: TypeInfo) = when (returnTypeInfo.type) {
+//    HttpStatement::class -> this
+//    HttpResponse::class -> execute()
+//    else -> execute { it.call.receive(returnTypeInfo) }
+//}
 
 inline fun <reified T> HttpClient.create(baseUrl: String? = null) =
-    dynamicProxyToHttpClient(T::class, this, baseUrl) as T
+    createDynamicProxyToHttpClient(this, T::class, baseUrl) as T
 
-private suspend inline fun HttpStatement.executeAndReceive(returnTypeInfo: TypeInfo) = when (returnTypeInfo.type) {
-    HttpStatement::class -> this
-    HttpResponse::class -> execute()
-    else -> execute { it.call.receive(returnTypeInfo) }
+@PublishedApi
+internal fun createDynamicProxyToHttpClient(httpClient: HttpClient, kClass: KClass<*>, baseUrl: String?): Any {
+    val jClass = kClass.java
+    //check interface
+    require(jClass.isInterface) { "API declaration must be interface" }
+    require(jClass.interfaces.isEmpty()) { "API interface must not extend other interfaces" }
+    //create proxy
+    return Proxy.newProxyInstance(
+        jClass.classLoader,
+        arrayOf(jClass),
+        CaeruleumProxy(httpClient, kClass, jClass, baseUrl)
+    )
 }
